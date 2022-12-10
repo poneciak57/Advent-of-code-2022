1c45806e05f879b07655bbf512d2202e0cd4186a
poneciak <poneciak@gmail.com>
2022-12-10 09:37:19 +0100

    Initial Commit 2

---
+++ Template/star1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main(){
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+
+
+
+    std::cout.flush();  
+    return 0;
+}
