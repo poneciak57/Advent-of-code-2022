2cd531605d50361b39c65205260e460149b3b565
poneciak <poneciak@gmail.com>
2022-12-10 09:36:50 +0100

    Day4 :star: :star:

---
+++ Day4/star1.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+// A quick way to split strings separated via any character
+// delimiter.
+std::vector<std::string> adv_tokenizer(std::string s, char del)
+{
+    std::vector<std::string> ret;
+    std::stringstream ss(s);
+    std::string word;
+    while (!ss.eof()) {
+        getline(ss, word, del);
+        ret.push_back(word);
+    }
+    return ret;
+}
+std::stringstream split(std::string str){
+    std::string temp = "";
+    std::stringstream ret;
+    for(int i=0; i<(int)str.size(); i++){
+        if(str[i] != ',' && str[i] != '-'){
+            temp += str[i];
+            continue;
+        }
+        ret << temp;
+        temp = " ";
+    }
+    ret<<temp;
+    return ret;
+}
+ 
+int main(int argc, char const* argv[])
+{
+    std::string inp;
+    int sum{0};
+    while(std::cin>>inp){
+        std::stringstream ss = split(inp + " ");
+        int e1p,e1e,e2p,e2e;
+        ss >> e1p >> e1e >> e2p >> e2e;
+        if((e1p<=e2p && e1e >= e2e) ||(e2p<=e1p && e2e >= e1e)){
+            sum++;
+        }
+    }
+    std::cout<<sum;
+    return 0;
+}
